6bd2c26ae4abf8ead6a10ab940e76944d87adb45
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2020-10-12 18:55:24 -0300

    ga added

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   css: ["~/assets/scss/styles.scss"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{ src: "~plugins/ga.js", mode: "client" }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
