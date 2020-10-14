c7a4c4dd88e2538a9d6871c83c392ee00f169158
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2020-10-14 07:57:48 -0300

    champion header mobile version

--- components/ChampionHeader.vue
+++ components/ChampionHeader.vue
@@ -24,26 +24,40 @@ export default {
 @import "./../assets/scss/utils.scss";
 
 .champion__header {
-  margin-top: 48px;
+  margin-top: 24px;
   margin-bottom: 12px;
+
+  @include breakpoint(sm) {
+    margin-top: 48px;
+  }
 }
 
 .champion__title {
   font-family: $font2;
   text-align: center;
-  font-size: 40px;
-  line-height: 54px;
+  font-size: 24px;
+  line-height: 32px;
   text-transform: uppercase;
+
+  @include breakpoint(sm) {
+    font-size: 40px;
+    line-height: 54px;
+  }
 }
 
 .champion__subtitle {
   font-family: $font2;
   text-align: center;
-  font-size: 50px;
-  line-height: 54px;
   display: block;
   color: $color_secondary;
   margin-bottom: 32px;
   text-transform: uppercase;
+  font-size: 32px;
+  line-height: 38px;
+
+  @include breakpoint(sm) {
+    font-size: 50px;
+    line-height: 54px;
+  }
 }
 </style>
